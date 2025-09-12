60024509c2bd352a20a95c0575d9d1fdc7a6a3bd
Anshulsharmacode <anshulsharma8386@gmail.com>
2025-09-12 22:50:08 +0530

    done the count and reverse

---
+++ basic/count.py
@@ -0,0 +1,11 @@
+n = 5873 
+
+num =n 
+count = 0
+while num>0:
+    last = num %10
+    count +=1
+    num = num //10
+
+print(count)
+    
---
+++ basic/reverse.py
@@ -0,0 +1,8 @@
+n=652375
+num =n
+while num>0:
+    last = num %10
+    num= num //10
+    print(last)
+
+print(num)
