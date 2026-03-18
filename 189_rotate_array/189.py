33e93a38616216a398eb262dadda9c45b2fa401c
Anshulsharmacode <anshulsharma8386@gmail.com>
2026-03-19 01:21:14 +0530

    done the 189 rotate array from k with brute solution

---
+++ 189_rotate_array/189.py
@@ -0,0 +1,13 @@
+# /Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+nums = [1,2,3,4,5,6,7]
+k = 3
+
+
+
+#brute for solution
+# lenth= len(nums)
+# for i in range(0, k):
+#     e= nums.pop()
+#     nums.insert(0,e)
+
+# print(nums)
